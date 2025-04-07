9701fcdf77467724a481618b57a47df24c6559a8
Serge Thomas <smughemuzi@gmail.com>
2025-04-07 11:28:34 +0200

    change function naming convention and added division function with test data/ function call

--- math_func.py
+++ math_func.py
@@ -1,10 +1,19 @@
-def calc(num1, num2):
+def mult_calc(num1, num2):
     return num1 * num2
+
+
+def divide_calc(num1, num2):
+    try:
+        return num1 / num2
+    except ZeroDivisionError:
+        print("Cannot divide by zero")
 
 
 num1 = int(input("Enter your first number: "))
 num2 = int(input("Enter your second number: "))
 
-calc_result = calc(num1, num2)
+mult_calc_result = mult_calc(num1, num2)
+divide_calc_result = divide_calc(num1, num2)
 
-print(f"Result of {num1} * {num2} is: {calc_result}")
+print(f"Result of {num1} * {num2} is: {mult_calc_result}")
+print(f"Result of {num1} / {num2} is: {divide_calc_result}")

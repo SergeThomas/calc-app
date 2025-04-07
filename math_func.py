1b506147b8383147b8a843886da41119f3d6222d
Serge Thomas <smughemuzi@gmail.com>
2025-04-07 11:15:24 +0200

    added user inputs and function call to test data

--- math_func.py
+++ math_func.py
@@ -1,2 +1,10 @@
 def calc(num1, num2):
     return num1 * num2
+
+
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+calc_result = calc(num1, num2)
+
+print(f"Result of {num1} * {num2} is: {calc_result}")
